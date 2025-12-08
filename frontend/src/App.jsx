eeaf10acedf7a26489fec0b2d17e85a3c379505d
Junsool <Leedjung913@gmail.com>
2025-12-07 18:01:59 -0600

    registration page and login page rough draft developed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login';
+import Register from './pages/Register';
 import MainPage from './pages/MainPage';
-import Home from './pages/Home';
+import Launch from './pages/Launch';
+import './index.css'
 
 function App() {
   return (
-    <>
+    <div className="app-body">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Launch />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
           <Route path="/MainPage" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Launch.jsx
@@ -0,0 +1,13 @@
+import './launch.css';
+
+function Launch() {
+    return(
+        <>
+            <h1>
+                THIS PAGE WILL BE FIRST DISPLAYED FOR NEW USERS
+            </h1>
+        </>
+    );
+}
+
+export default Launch;
