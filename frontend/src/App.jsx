500141b1f88794c1268adc5511f97ca2de306e7c
andrewjeon <aj55@illinois.edu>
2025-12-07 18:20:55 -0600

    Rough text editor and a fixed weight for editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './pages/Login';
 import MainPage from './pages/MainPage';
+import EditPage from "./pages/EditPage";
 import Home from './pages/Home';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/MainPage" element={<MainPage />} />
+          <Route path="/edit/:noteId" element={<EditPage />} />
         </Routes>
       </BrowserRouter>
     </>
