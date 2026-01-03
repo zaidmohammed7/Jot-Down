130ed5d19a2fc64524d516105301cbb7ca1cb151
Zaid Khan Mohammed <zaidkhanmd27@gmail.com>
2026-01-02 21:23:27 -0600

    Matched repo name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/jot-down">
+    <BrowserRouter basename="/Jot-Down">
       <App />
       </BrowserRouter>
   </StrictMode>,
